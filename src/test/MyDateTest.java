e236bffbb895e016a60bd3fb55291ce7fcdfa947
Tommy-La <tien.la@edu.sait.ca>
2023-11-02 19:10:37 -0500

    Fixed MyDateTest
    Fixed MyDateTest.java in test package

--- src/test/MyDateTest.java
+++ src/test/MyDateTest.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,34 +8,55 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 
+ */
 class MyDateTest {
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
-	@Test
-	void testSetDate() {
-		fail("Not yet implemented");
-	}
-
+	/**
+	 * Test method for {@link dateADT.MyDate#compareTo(dateADT.DateADT)}.
+	 */
 	@Test
 	void testCompareTo() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link dateADT.MyDate#toISOFormat()}.
+	 */
 	@Test
 	void testToISOFormat() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link dateADT.MyDate#advanceByDays(int)}.
+	 */
 	@Test
 	void testAdvanceByDays() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link dateADT.MyDate#retreatByDays(int)}.
+	 */
 	@Test
 	void testRetreatByDays() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dateADT.MyDate#MyDate(int, int, int)}.
+	 */
+	@Test
+	void testMyDate() {
 		fail("Not yet implemented");
 	}
 
